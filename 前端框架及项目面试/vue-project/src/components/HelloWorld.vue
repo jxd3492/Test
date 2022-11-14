7a95f95e0096fa8ae6cfd5ca20a6512687550981
jxd3492 <84827385+jxd3492@users.noreply.github.com>
2022-11-14 14:23:40 +0800

    123

--- 前端框架及项目面试/vue-project/src/components/HelloWorld.vue
+++ 前端框架及项目面试/vue-project/src/components/HelloWorld.vue
@@ -1,9 +1,15 @@
 <template>
   <TopView />
+  <MapView />
+  <SalesView />
+  <ButtonView />
 </template>
 
 <script setup>
 import TopView from './TopView/index.vue'
+import ButtonView from './ButtonView/index.vue'
+import SalesView from './SalesView/index.vue'
+import MapView from './MapView/index.vue'
 </script>
 
 <style scoped>
